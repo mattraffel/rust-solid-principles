12eed87ba5d40bfd586bbbaf4d351e41d9dd245f
matt raffel <matt.raffel@evernym.com>
2018-05-25 13:41:22 -0600

    no message

---
+++ code/src/engine/mod.rs
@@ -0,0 +1,12 @@
+//!
+
+
+pub struct Engine {
+
+}
+
+impl Engine {
+    pub fn run(&self) {
+        println!("the engine is running");
+    }
+}
--- code/src/main.rs
+++ code/src/main.rs
@@ -1,6 +1,13 @@
 //!
+//! The goal of this project is to show all of the principles of SOLID being used, particularly
+//! focusing on LID part of SOLID.
 //!
+//! The demonstration contains an engine that gets data and runs several computations on the data
 //!
+//! The SOLID principles will be demonstrated by changing the source of the data and using multiple
+//! computations WITHOUT any changes to the engine.
+
+#![allow(unused_imports)]
 
 extern crate ansi_term;
 extern crate env_logger;
@@ -10,6 +17,14 @@ extern crate libc;
 #[macro_use] extern crate log;
 
 
+mod data_sources;
+mod engine;
+
+use engine::Engine;
+
 fn main() {
-    println!("Hello, world!");
+
+    let e: Engine = Engine { };
+    e.run();
+
 }
