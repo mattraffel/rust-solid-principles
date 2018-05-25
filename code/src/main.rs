03c29ec595c5be01d7b8fad661a9d68c67338b29
matt raffel <matt.raffel@evernym.com>
2018-05-25 13:17:03 -0600

    no message

---
+++ code/src/main.rs
@@ -0,0 +1,15 @@
+//!
+//!
+//!
+
+extern crate ansi_term;
+extern crate env_logger;
+extern crate libc;
+
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate log;
+
+
+fn main() {
+    println!("Hello, world!");
+}
