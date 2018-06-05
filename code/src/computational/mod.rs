9df803d6215f68ec23d3fc948041233bd8ba12b6
matt raffel <matt.raffel@evernym.com>
2018-06-05 14:06:21 -0600

    no message

--- code/src/computational/mod.rs
+++ code/src/computational/mod.rs
@@ -1,7 +1,11 @@
 //!
 
-pub mod closest_values;
+// these mods are pub so that others can use them
 pub mod computations;
 pub mod factory;
-pub mod least_to_greatest;
-pub mod maximum_value;
+
+// these mods are not.  because part of SOLID is hiding implementation details
+// from the consumers, these should not be pub
+mod closest_values;
+mod least_to_greatest;
+mod maximum_value;
--- code/src/data_sources/mod.rs
+++ code/src/data_sources/mod.rs
@@ -3,9 +3,13 @@
 #![allow(dead_code)]
 #![allow(non_snake_case)]
 
+// these mods are pub so that others can use them
 pub mod factory;
 pub mod data_source;
-pub mod in_memory_data_source;
-pub mod mysql;
+
+// these mods are not.  because part of SOLID is hiding implementation details
+// from the consumers, these should not be pub
+mod in_memory_data_source;
+mod mysql;
 
 
